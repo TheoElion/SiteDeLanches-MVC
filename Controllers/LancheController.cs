05d7d3d42403d8ccdcdfeff0e892ea69e14f1eb5
Theo Elion <theoelion54@gmail.com>
2025-04-12 23:53:03 -0300

    Lista de Lanches

---
+++ Controllers/LancheController.cs
@@ -0,0 +1,25 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers;
+
+public class LancheController : Controller
+{
+
+    private readonly ILanchesRepository _lancheRepository;
+
+    public LancheController(ILanchesRepository lancheRepository)
+    {
+        _lancheRepository = lancheRepository;
+    }
+
+    public IActionResult List()
+    {
+        var lanchesListViewModel = new LancheListViewModel();
+        lanchesListViewModel.Lanches = _lancheRepository.Lanches;
+        lanchesListViewModel.CategoriaAtual = "Categoria atual";
+
+        return View(lanchesListViewModel);
+    }
+}
