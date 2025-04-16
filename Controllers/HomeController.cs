d47f6910a73ee1930dae408f356dbb974ae266d2
Theo Elion <theoelion54@gmail.com>
2025-04-15 22:29:18 -0300

    Lista de lanches

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 using System.Diagnostics;
 using LanchesMac.Models;
+using LanchesMac.Repositories;
+using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
@@ -7,10 +10,21 @@ namespace LanchesMac.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ILanchesRepository _lancheRepository;
+
+        public HomeController(ILanchesRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
 
         public IActionResult Index()
         {
-            return View();
+            var homeViewModel = new HomeViewModel
+            {
+                LanchesPreferidos = _lancheRepository.LanchesPreferidos
+            };
+
+            return View(homeViewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Controllers/LancheController.cs
+++ Controllers/LancheController.cs
@@ -1,4 +1,5 @@
-﻿using LanchesMac.Repositories.Interfaces;
+﻿using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
 using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,12 +15,36 @@ public class LancheController : Controller
         _lancheRepository = lancheRepository;
     }
 
-    public IActionResult List()
+    public IActionResult List(string categoria)
     {
-        var lanchesListViewModel = new LancheListViewModel();
-        lanchesListViewModel.Lanches = _lancheRepository.Lanches;
-        lanchesListViewModel.CategoriaAtual = "Categoria atual";
+        IEnumerable<Lanche> lanches;
+        string categoriaAtual = string.Empty;
+
+        if (string.IsNullOrEmpty(categoria))
+        {
+            lanches = _lancheRepository.Lanches.OrderBy(l => l.LancheId);
+            categoriaAtual = "Todos os lanches";
+        }
+        else
+        {
+            lanches = _lancheRepository.Lanches
+                .Where(l => l.Categoria.CategoriaNome.Equals(categoria))
+                .OrderBy(c => c.Nome);
+
+            categoriaAtual = categoria;
+        }
+        var lanchesListViewModel = new LancheListViewModel
+        {
+            Lanches = lanches,
+            CategoriaAtual = categoriaAtual,
+        };
 
         return View(lanchesListViewModel);
+    }
+
+    public IActionResult Details(int lancheId)
+    {
+        var lanche = _lancheRepository.Lanches.FirstOrDefault(l => l.LancheId == lancheId);
+        return View(lanche);
     }
 }
