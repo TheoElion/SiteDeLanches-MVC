ed074fefef4a22fd9bfcf6a9f803042ffbcc6756
Theo Elion <theoelion54@gmail.com>
2025-04-16 14:41:31 -0300

    Pedido / Checkout

---
+++ Controllers/PedidoController.cs
@@ -0,0 +1,59 @@
+﻿using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers;
+
+public class PedidoController : Controller
+{
+    private readonly IPedidoRepository _pedidoRepository;
+    private readonly CarrinhoCompra _carrinhoCompra;
+
+    public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra)
+    {
+        _pedidoRepository = pedidoRepository;
+        _carrinhoCompra = carrinhoCompra;
+    }
+
+    public IActionResult Checkout()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Checkout(Pedido pedido)
+    {
+        int totalItensPedido = 0;
+        decimal precoTotalDoPedido = 0.0m;
+
+        List<CarrinhoCompraItem> itens = _carrinhoCompra.GetCarrinhoCompraItens();
+        _carrinhoCompra.CarrinhoCompraItens = itens;
+
+        if (_carrinhoCompra.CarrinhoCompraItens.Count() == 0)
+        {
+            ModelState.AddModelError("", "Seu carinho esta vazio, que tal incluir um lanche...");
+        }
+
+        foreach (var item in itens)
+        {
+            totalItensPedido += item.Quantidade;
+            precoTotalDoPedido += item.Lanche.Preco * item.Quantidade;
+        }
+
+        pedido.TotalItensPedido = totalItensPedido;
+        pedido.PedidoTotal = precoTotalDoPedido;
+
+        if (ModelState.IsValid)
+        {
+            _pedidoRepository.CriarPedido(pedido);
+
+            ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido XD";
+            ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+            _carrinhoCompra.LimparCarrinho();
+
+            return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+        }
+        return View(pedido);
+    }
+}
---
+++ Repositories/PedidoRepository.cs
@@ -0,0 +1,39 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+
+namespace LanchesMac.Repositories;
+
+public class PedidoRepository : IPedidoRepository
+{
+    private readonly AppDbConxtex _appDbConxtex;
+    private readonly CarrinhoCompra _carrinhoCompra;
+
+    public PedidoRepository(AppDbConxtex appDbConxtex, CarrinhoCompra carrinhoCompra)
+    {
+        _appDbConxtex = appDbConxtex;
+        _carrinhoCompra = carrinhoCompra;
+    }
+
+    public void CriarPedido(Pedido pedido)
+    {
+        pedido.PedidoEnviado = DateTime.Now;
+        _appDbConxtex.Pedidos.Add(pedido);
+        _appDbConxtex.SaveChanges();
+
+        var carrinhoCompraItens = _carrinhoCompra.CarrinhoCompraItens;
+
+        foreach (var carrinhoItem in carrinhoCompraItens)
+        {
+            var pedidoDetail = new PedidoDetalhe()
+            {
+                Quantidade = carrinhoItem.Quantidade,
+                LancheId = carrinhoItem.Lanche.LancheId,
+                PedidoId = pedido.PedidoId,
+                Preco = carrinhoItem.Lanche.Preco
+            };
+            _appDbConxtex.PedidoDetalhes.Add(pedidoDetail);
+        }
+        _appDbConxtex.SaveChanges();
+    }
+}
